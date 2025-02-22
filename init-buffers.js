a380e95b381cd0cfb7d7b48b82d659bdace66ba8
backpack <bdm0227@gmail.com>
2025-02-22 18:26:44 +0900

    Create init-buffers.js

---
+++ init-buffers.js
@@ -0,0 +1,29 @@
+    12,
+    14,
+    15, // bottom
+    16,
+    17,
+    18,
+    16,
+    18,
+    19, // right
+    20,
+    21,
+    22,
+    20,
+    22,
+    23, // left
+  ];
+
+  // Now send the element array to GL
+
+  gl.bufferData(
+    gl.ELEMENT_ARRAY_BUFFER,
+    new Uint16Array(indices),
+    gl.STATIC_DRAW
+  );
+
+  return indexBuffer;
+}
+
+export { initBuffers };
