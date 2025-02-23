d44da238df5886dd467f2ad8c79e12a9d13b214e
backpack <bdm0227@gmail.com>
2025-02-23 14:34:40 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
 
 function main()
 {
-      alert("Init");
+      const canvas = document.querySelector("canvas");
+
+      if (!navigator.gpu) {
+          throw new Error("WebGPU not supported on this browser.");
+      }
+
+      const adapter = await navigator.gpu.requestAdapter();
+      if (!adapter) {
+          throw new Error("No appropriate GPUAdapter found.");
+      }
+
+      const device = await adapter.requestDevice();
+
+      const context = canvas.getContext("webgpu");
+      const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+      context.configure({
+          device: device,
+          format: canvasFormat,
+      });
+
+      const encoder = device.createCommandEncoder();
+
+      const pass = encoder.beginRenderPass({
+          colorAttachments: [{
+                  view: context.getCurrentTexture().createView(),
+                  loadOp: "clear",
+                  storeOp: "store",
+              }]
+      });
+
+      pass.end();
+
+      device.queue.submit([encoder.finish()]);
 }
 
