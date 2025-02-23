697e21658d041342a14b4d12109d0287f1d7c703
backpack <bdm0227@gmail.com>
2025-02-23 16:37:47 +0900

    Update Common.js

--- Common.js
+++ Common.js
@@ -1,6 +1,6 @@
 
 function print(text)
 {
-  return document.getElementById("#print").insertAdjacentText("beforeend", "Something Working");
+  return document.getElementById("#print").insertAdjacentText("beforeend", text);
 }
 
