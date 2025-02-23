fda91aea5bd2a3ca0856201373e916c0d2de8b46
backpack <bdm0227@gmail.com>
2025-02-23 15:48:58 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,7 @@ function main()
 {
       const canvas = document.querySelector("#canvas");
 
-      if (!navigator.gpu) {
-          throw new Error("WebGPU not supported on this browser.");
-      }
-
       const adapter = await navigator.gpu.requestAdapter();
-      if (!adapter) {
-          throw new Error("No appropriate GPUAdapter found.");
-      }
-
       const device = await adapter.requestDevice();
 
       const context = canvas.getContext("webgpu");
