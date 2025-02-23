9a4322df80aecbdc1265c69b234a0565c61ae18e
backpack <bdm0227@gmail.com>
2025-02-23 18:48:46 +0900

    Update initialize.js

--- initialize.js
+++ initialize.js
@@ -1,21 +1,21 @@
 
 function initForAll()
 {
-      const canvas = document.querySelector("#canvas");
+      let canvas = document.querySelector("#canvas");
 
-      const adapter = await navigator.gpu.requestAdapter();
-      const device = await adapter.requestDevice();
+      let adapter = await navigator.gpu.requestAdapter();
+      let device = await adapter.requestDevice();
 
-      const context = canvas.getContext("webgpu");
-      const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+      let context = canvas.getContext("webgpu");
+      let canvasFormat = navigator.gpu.getPreferredCanvasFormat();
       context.configure({
           device: device,
           format: canvasFormat,
       });
 
-      const encoder = device.createCommandEncoder();
+      let encoder = device.createCommandEncoder();
 
-      const pass = encoder.beginRenderPass({
+      let pass = encoder.beginRenderPass({
           colorAttachments: [{
                   view: context.getCurrentTexture().createView(),
                   loadOp: "clear",
