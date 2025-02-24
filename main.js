371f4b0b5a8896636ed4190914c4a807be175309
backpack <bdm0227@gmail.com>
2025-02-24 17:47:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 //import initForAll from "./initialize.js";
 
+function prtById(id, text)
+{
+  document.getElementById(id).insertAdjacentText("beforeend", text);
+}
+
 function main()
 {
+  prtById("print", "ff");
   //initForAll();
-  document.getElementById("print").insertAdjacentText("beforeend", "dd");
+  //document.getElementById("print").insertAdjacentText("beforeend", "dd");
 }
 
 main();
