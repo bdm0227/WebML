81eb89f797dffdc0b95056ed39ad0e70a00335f4
backpack <bdm0227@gmail.com>
2025-02-25 18:23:27 +0900

    Update common.js

--- common.js
+++ common.js
@@ -1,7 +1,7 @@
 
 function prtById(id, text)
 {
-  document.getElementId(id).insertAdjacentText("beforeend", text);
+  document.getElementById(id).insertAdjacentText("beforeend", text);
 }
 
 export { prtById };
