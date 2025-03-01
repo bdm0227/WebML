c59a7c54dce04c85e25d316c5ba2b59a7906980c
backpack <bdm0227@gmail.com>
2025-03-01 18:25:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ import * as cmm from "./common.js";
 function main()
 {
   const canvas = document.querySelector("#canvas");
-  /**
-      const adapter = await navigator.gpu.requestAdapter();
-      const device = await adapter.requestDevice();
+  
+  const adapter = await navigator.gpu.requestAdapter();
+  const device = await adapter.requestDevice();
 
-      const context = canvas.getContext("webgpu");
+      /**const context = canvas.getContext("webgpu");
       const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
       context.configure({
           device: device,
