ed6c69549ecb46ceccaba5a9de1153ec1061a7ff
backpack <bdm0227@gmail.com>
2025-03-02 16:00:58 +0900

    Update resource.js

--- resource.js
+++ resource.js
@@ -1,1 +1,23 @@
 
+function createResource(buffer)
+{
+  
+}
+
+function updateResource(buffer, src)
+{
+  const gpuBuffer = device.createBuffer({
+    mappedAtCreation: true,
+    size: 4,
+    usage: GPUBufferUsage.MAP_WRITE
+  });
+  const arrayBuffer = buffer.getMappedRange();
+
+  new Uint8Array(arrayBuffer).set([0, 1, 2, 3]);
+}
+
+function readbackResource(buffer, dst)
+{
+  
+}
+
