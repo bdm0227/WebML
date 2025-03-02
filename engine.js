3c34119fb2bc76bf44c2dd59f95087716a71acac
backpack <bdm0227@gmail.com>
2025-03-02 20:55:07 +0900

    Update engine.js

--- engine.js
+++ engine.js
@@ -1,6 +1,32 @@
 
 function Engine()
 {
+  this.Init = async function() {
+    const canvas = document.querySelector("#canvas");
   
+    const adapter = await navigator.gpu.requestAdapter();
+    const device = await adapter.requestDevice();
+  
+    const context = canvas.getContext("webgpu");
+    const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+    context.configure({
+        device: device,
+        format: canvasFormat,
+    });
+
+    const encoder = device.createCommandEncoder();
+
+    const pass = encoder.beginRenderPass({
+        colorAttachments: [{
+                view: context.getCurrentTexture().createView(),
+                loadOp: "clear",
+                storeOp: "store",
+            }]
+    });
+
+    pass.end();
+
+    device.queue.submit([encoder.finish()]);
+  }
 }
 
