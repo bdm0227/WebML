684529ad676a3010f7a9e632c91305cee69abf4c
backpack <bdm0227@gmail.com>
2025-03-03 17:54:39 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as cmm from "./common.js";
 //import { Engine } from "./engine.js";
 
-async function main()
+function main()
 {
   //let engine = new Engine();
   cmm.prtById("print1", "It works!");
