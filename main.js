d0f72c42bb91cb24c5f37de0d8c9e0b17d9a8451
backpack <bdm0227@gmail.com>
2025-03-03 18:00:27 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
 import * as cmm from "./common.js";
-//import { Engine } from "./engine.js";
 
-function main()
+async function main()
 {
-  //let engine = new Engine();
+  const canvas = document.querySelector("#canvas");
+  
+  const adapter = await navigator.gpu.requestAdapter();
+  const device = await adapter.requestDevice();
+  
+  const context = canvas.getContext("webgpu");
+  const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
+  context.configure({
+      device: device,
+      format: canvasFormat,
+  });
+
+  const encoder = device.createCommandEncoder();
+
+  const pass = encoder.beginRenderPass({
+      colorAttachments: [{
+              view: context.getCurrentTexture().createView(),
+              loadOp: "clear",
+              storeOp: "store",
+          }]
+  });
+
+  pass.end();
+
+  device.queue.submit([encoder.finish()]);
+  
   cmm.prtById("print1", "It works!");
 }
 
