db49718d0e363206d15e625406e8e16a437ad5ea
backpack <bdm0227@gmail.com>
2025-04-06 18:08:13 +0900

    Update main.js

--- main.js
+++ main.js
@@ -16,6 +16,17 @@ async function main()
       format: canvasFormat,
   });
 
+  const vertices = new Float32Array([
+    //   X,    Y,
+    -0.8, -0.8, // Triangle 1 (Blue)
+    0.8, -0.8,
+    0.8,  0.8,
+
+   -0.8, -0.8, // Triangle 2 (Red)
+    0.8,  0.8,
+   -0.8,  0.8,
+  ]);
+
   const encoder = device.createCommandEncoder();
 
   const pass = encoder.beginRenderPass({
