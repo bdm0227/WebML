39b898d26eb047212f92e6d8ba47c891b1ca61c0
backpack <bdm0227@gmail.com>
2025-04-06 19:01:58 +0900

    Update main.js

--- main.js
+++ main.js
@@ -15,24 +15,6 @@ async function main()
       device: device,
       format: canvasFormat,
   });
-/*
-  const vertices = new Float32Array([
-    //   X,    Y,
-    -0.8, -0.8, // Triangle 1 (Blue)
-    0.8, -0.8,
-    0.8,  0.8,
-
-   -0.8, -0.8, // Triangle 2 (Red)
-    0.8,  0.8,
-   -0.8,  0.8
-  ]);
-  /*
-  const vertexBuffer = device.createBuffer({
-    label: "Cell vertices",
-    size: vertices.byteLength,
-    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
-  });
-  device.queue.writeBuffer(vertexBuffer, 0, vertices);*/
 
   const encoder = device.createCommandEncoder();
 
